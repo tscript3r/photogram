1f3c343b75a65e8e8bf88f633fb95208fe092b13
tscript3r <alek199202@gmail.com>
2019-10-15 16:12:04 +0200

    added java email configuration class

--- backend/src/main/java/pl/tscript3r/photogram/infrastructure/configuration/EmailConfig.java
+++ backend/src/main/java/pl/tscript3r/photogram/infrastructure/configuration/EmailConfig.java
@@ -37,7 +37,6 @@ public class EmailConfig {
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
 
         return mailSender;
